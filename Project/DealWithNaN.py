097c29ec02d923853e21ec7e605da177755edd3d
Andrei-Eugen Birta <1062021@ucn.dk>
2018-04-15 18:50:29 +0200

    added: 	Files are automatically detected (no need to change the directory anymore) modified: 	Improved code running time by running things in fonctions rather than globaly
    Signed-off-by: Andrei-Eugen Birta <1062021@ucn.dk>

--- Project/DealWithNaN.py
+++ Project/DealWithNaN.py
@@ -7,17 +7,23 @@ from sklearn.preprocessing import Imputer
 import matplotlib.pyplot as plt
 import os
 
-os.chdir('C:/Users/bubri/Desktop/Project/dataset/Modified_dataset/') # Set working directory
-
-df = pd.read_csv('wildlife-collisions-Modified.csv',
-                 dtype={'INCIDENT_DATE': object, 'ATYPE': object, 'OPERATOR': object, 'AC_CLASS': object, 
-                        'AC_MASS': float, 'NUM_ENGS': float, 'TYPE_ENG': object, 'AIRPORT': object, 
-                        'STATE': object, 'ENROUTE': object, 'HEIGHT': float, 'SPEED': float, 
-                        'DISTANCE': float, 'PHASE_OF_FLT': object, 'DAMAGE': object, 'STR_RAD': int, 
-                        'DAM_RAD': int, 'STR_WINDSHLD': int, 'DAM_WINDSHLD': int, 'STR_NOSE': int, 
-                        'DAM_NOSE': int, 'STR_ENG1': int, 'DAM_ENG1': int, 'STR_ENG2': int, 
-                        'DAM_ENG2': int, 'STR_ENG3': int, 'DAM_ENG3': int, 'STR_ENG4': int, 
-                        'DAM_ENG4': int, 'INGESTED': int, 'STR_PROP': int, 'DAM_PROP': int, 
+######################################## FIND FILES ##########################################
+def find(name): #finds needed files in pc
+    for root, dirs, files in os.walk('C:\\Users'):
+        if name in files:
+            print('Files Found')
+            return root
+        
+def readFile():
+    df = pd.read_csv('wildlife-collisions-Modified.csv',
+                 dtype={'INCIDENT_DATE': object, 'OPERATOR': object, 'ATYPE': object, 'AMA': object,
+                        'EMA': object, 'AC_CLASS': object, 'AC_MASS': float, 'NUM_ENGS': float, 
+                        'TYPE_ENG': object, 'AIRPORT': object, 'STATE': object, 'ENROUTE': object, 
+                        'HEIGHT': float, 'SPEED': float, 'DISTANCE': float, 'PHASE_OF_FLT': object, 
+                        'DAMAGE': object, 'STR_RAD': int, 'DAM_RAD': int, 'STR_WINDSHLD': int, 
+                        'DAM_WINDSHLD': int, 'STR_NOSE': int, 'DAM_NOSE': int, 'STR_ENG1': int, 
+                        'DAM_ENG1': int, 'STR_ENG2': int, 'DAM_ENG2': int, 'STR_ENG3': int, 'DAM_ENG3': int, 
+                        'STR_ENG4': int, 'DAM_ENG4': int, 'INGESTED': int, 'STR_PROP': int, 'DAM_PROP': int, 
                         'STR_WING_ROT': int, 'DAM_WING_ROT': int, 'STR_FUSE': int, 'DAM_FUSE': int, 
                         'STR_LG': int, 'DAM_LG': int, 'STR_TAIL': int, 'DAM_TAIL': int, 'STR_LGHTS': int,
                         'DAM_LGHTS': int, 'STR_OTHER': int, 'DAM_OTHER': int, 'EFFECT': object, 
@@ -27,16 +33,14 @@ df = pd.read_csv('wildlife-collisions-Modified.csv',
                         'NR_FATALITIES': float, 'INDICATED_DAMAGE': int},
                 parse_dates=['INCIDENT_DATE'])
 
-df = df.set_index('INCIDENT_DATE')
+    df = df.set_index('INCIDENT_DATE')
+    return df
 
-'''#dont delete was used for deciding how to fill nan values
-test = df['SKY']
-
-#AIRPORT contains also from other contries and that could explain big part of states missing but not all
-#Maybe remove column- BIRDS_SEEN WARNED
-#Maybe remove row- States
-#Maybe use mode- PHASE_OF_FLT SKY
-
+'''dont delete was used for deciding how to fill nan values
+test = df['WARNED']
+#No ATYPE EMA AIRPORT STATE
+#Maybe remove BIRDS_SEEN WARNED
+#Maybe AMA PHASE_OF_FLT SKY
 print(test.value_counts())
 
 print(test.isnull().sum())
@@ -61,26 +65,29 @@ plt.vlines(test.median(),   # Plot red line at median
            linewidth=2.0,
            color="red")
 '''
-#Merge these fields to show state where the incident happened
-df['ENROUTE'].fillna(df['STATE'], inplace=True)
-df = df.drop(['STATE'], axis=1)
-df = df.rename(columns={'ENROUTE': 'STATE'})
+def mergeFields(df): #Merge these fields to show state where the incident happened
+    df['ENROUTE'].fillna(df['STATE'], inplace=True)
+    df = df.drop(['STATE'], axis=1)
+    df = df.rename(columns={'ENROUTE': 'STATE'})
+    return df
 
-#Fills nan values with value
-df[['OPERATOR', 'AIRPORT', 'ATYPE']] = df[['OPERATOR', 'AIRPORT', 'ATYPE']].fillna('Unknown')
-df[['NR_INJURIES', 'NR_FATALITIES']] = df[['NR_INJURIES', 'NR_FATALITIES']].fillna(0)
+def fillNAwithValue(df): #Fills nan values with value
+    df['OPERATOR'].fillna('Unknown', inplace=True)
+    df['NR_INJURIES'].fillna(0, inplace=True)
+    df['NR_FATALITIES'].fillna(0, inplace=True)
+    
+    #Fills nan values using most popular
+    df['SIZE'].fillna(df['SIZE'].value_counts().idxmax(), inplace=True)
+    df['BIRDS_STRUCK'].fillna(df['BIRDS_STRUCK'].value_counts().idxmax(), inplace=True)
+    df['SPECIES'].fillna(df['SPECIES'].value_counts().idxmax(), inplace=True)
+    df['PRECIP'].fillna(df['PRECIP'].value_counts().idxmax(), inplace=True)
+    df['EFFECT'].fillna(df['EFFECT'].value_counts().idxmax(), inplace=True)
+    df['DAMAGE'].fillna(df['DAMAGE'].value_counts().idxmax(), inplace=True)
+    df['AC_CLASS'].fillna(df['AC_CLASS'].value_counts().idxmax(), inplace=True)
+    df['TYPE_ENG'].fillna(df['TYPE_ENG'].value_counts().idxmax(), inplace=True)
+    return df
 
-#Fills nan values using most popular
-df['SIZE'].fillna(df['SIZE'].value_counts().idxmax(), inplace=True)
-df['BIRDS_STRUCK'].fillna(df['BIRDS_STRUCK'].value_counts().idxmax(), inplace=True)
-df['SPECIES'].fillna(df['SPECIES'].value_counts().idxmax(), inplace=True)
-df['PRECIP'].fillna(df['PRECIP'].value_counts().idxmax(), inplace=True)
-df['EFFECT'].fillna(df['EFFECT'].value_counts().idxmax(), inplace=True)
-df['DAMAGE'].fillna(df['DAMAGE'].value_counts().idxmax(), inplace=True)
-df['AC_CLASS'].fillna(df['AC_CLASS'].value_counts().idxmax(), inplace=True)
-df['TYPE_ENG'].fillna(df['TYPE_ENG'].value_counts().idxmax(), inplace=True)
-
-def fillNaNValues(columns, strategy):
+def fillNaNValues(df, columns, strategy):
     change = df[columns]#Columns to change
     
     impute = Imputer(missing_values='NaN',#Change NaN values
@@ -95,13 +102,20 @@ def fillNaNValues(columns, strategy):
     
     df[columns] = imputed[columns]#Replaces existing columns and all information in them with new
 
-fillNaNValues(['SPEED'], 'mean')#Used only if outliners have small or no influence
-fillNaNValues(['AC_MASS', 'NUM_ENGS'], 'most_frequent')#When clear correlation can be seen
-fillNaNValues(['HEIGHT', 'DISTANCE', 'AOS', 'COST_OTHER', 'COST_REPAIRS'], 'median')#To resist the effects of outliers
 
-print(df.isnull().sum())
-print(df.shape)
 
-#Writes to new file
-df.to_csv('wildlife-collisions-Finished.csv', encoding = "utf-8")
-print('Done!')
+
+########################################## MAIN FUNCTION ####################################
+def main():
+    os.chdir(find('wildlife-collisions.csv')) #set working directory
+    df=readFile()
+    fillNaNValues(df, ['SPEED'], 'mean')#Used only if outliners have small or no influence
+    fillNaNValues(df, ['AC_MASS', 'NUM_ENGS'], 'most_frequent')#When clear correlation can be seen
+    fillNaNValues(df, ['HEIGHT', 'DISTANCE', 'AOS', 'COST_OTHER', 'COST_REPAIRS'], 'median')#To resist the effects of outliers
+    df.to_csv(path_or_buf='wildlife-collisions-Modified.csv', encoding = "utf-8") #Writes to new file
+    print('Done!')
+    print(df.isnull().sum())
+    print(df.shape)
+    
+if __name__ == '__main__': #when program starts, start with main function
+    main()
--- Project/TransformData.py
+++ Project/TransformData.py
@@ -7,139 +7,160 @@ import numpy as np
 import os
 pd.options.mode.chained_assignment = None  # default='warn'
 
-os.chdir('C:/Users/bubri/Desktop/Project/dataset/Modified_dataset/') # Set working directory
 
-df = pd.read_csv('wildlife-collisions.csv',
-                 dtype={'INDEX_NR': int, 'OPID': object, 'OPERATOR': object, 'ATYPE': object, 'AMA': object,
-                        'AMO': object, 'EMA': object, 'EMO': object, 'AC_CLASS': object, 'AC_MASS': float, 
-                        'NUM_ENGS': float, 'TYPE_ENG': object, 'ENG_1_POS': object, 'ENG_2_POS': float, 'ENG_3_POS': object, 
-                        'ENG_4_POS': float, 'REG': object, 'FLT': object, 'REMAINS_COLLECTED': int, 'REMAINS_SENT': int, 
-                        'INCIDENT_DATE': object, 'INCIDENT_MONTH': float, 'INCIDENT_YEAR': float, 'TIME_OF_DAY': object, 'TIME': float, 
-                        'AIRPORT_ID': object, 'AIRPORT': object, 'STATE': object, 'FAAREGION': object, 'ENROUTE': object, 
-                        'RUNWAY': object, 'LOCATION': object, 'HEIGHT': float, 'SPEED': float, 'DISTANCE': float, 
-                        'PHASE_OF_FLT': object, 'DAMAGE': object, 'STR_RAD': int, 'DAM_RAD': int, 'STR_WINDSHLD': int, 
-                        'DAM_WINDSHLD': int, 'STR_NOSE': int, 'DAM_NOSE': int, 'STR_ENG1': int, 'DAM_ENG1': int, 
-                        'STR_ENG2': int, 'DAM_ENG2': int, 'STR_ENG3': int, 'DAM_ENG3': int, 'STR_ENG4': int, 
-                        'DAM_ENG4': int, 'INGESTED': int, 'STR_PROP': int, 'DAM_PROP': int, 'STR_WING_ROT': int, 
-                        'DAM_WING_ROT': int, 'STR_FUSE': int, 'DAM_FUSE': int, 'STR_LG': int, 'DAM_LG': int,
-                        'STR_TAIL': int, 'DAM_TAIL': int, 'STR_LGHTS': int, 'DAM_LGHTS': int, 'STR_OTHER': int,
-                        'DAM_OTHER': int, 'OTHER_SPECIFY': object, 'EFFECT': object, 'EFFECT_OTHER': object, 'SKY': object,
-                        'PRECIP': object, 'SPECIES_ID': object, 'SPECIES': object, 'BIRDS_SEEN': object, 'BIRDS_STRUCK': object,
-                        'SIZE': object, 'WARNED': object, 'COMMENTS': object, 'REMARKS': object, 'AOS': float,
-                        'COST_REPAIRS': float, 'COST_OTHER': float, 'COST_REPAIRS_INFL_ADJ': float, 'COST_OTHER_INFL_ADJ': float, 'REPORTED_NAME': object,
-                        'REPORTED_TITLE': object, 'REPORTED_DATE': object, 'SOURCE': object, 'PERSON': object, 'NR_INJURIES': float,
-                        'NR_FATALITIES': float, 'LUPDATE': object, 'TRANSFER': int, 'INDICATED_DAMAGE': int})
+######################################## FIND FILES ##########################################
+def find(name): #finds needed files in pc
+    for root, dirs, files in os.walk('C:\\Users'):
+        if name in files:
+            print('Files Found')
+            return root
 
-#replaces bad values with nan
-df = df.replace('', np.nan, regex=True)
-df = df.replace('UNKNOWN', np.nan, regex=True)
-df = df.replace('[Uu]nknown', np.nan, regex=True)
-df = df.replace('UNK', np.nan, regex=True)
-df = df.replace('CHANGE CODE', np.nan, regex=True)
-df = df.replace('ZZZZ', np.nan, regex=True)
+####################################### READ FILES ###########################################
+def readFiles():
+    df = pd.read_csv('wildlife-collisions.csv',
+                     dtype={'INDEX_NR': int, 'OPID': object, 'OPERATOR': object, 'ATYPE': object, 'AMA': object,
+                            'AMO': object, 'EMA': object, 'EMO': object, 'AC_CLASS': object, 'AC_MASS': float, 
+                            'NUM_ENGS': float, 'TYPE_ENG': object, 'ENG_1_POS': object, 'ENG_2_POS': float, 'ENG_3_POS': object, 
+                            'ENG_4_POS': float, 'REG': object, 'FLT': object, 'REMAINS_COLLECTED': int, 'REMAINS_SENT': int, 
+                            'INCIDENT_DATE': object, 'INCIDENT_MONTH': float, 'INCIDENT_YEAR': float, 'TIME_OF_DAY': object, 'TIME': float, 
+                            'AIRPORT_ID': object, 'AIRPORT': object, 'STATE': object, 'FAAREGION': object, 'ENROUTE': object, 
+                            'RUNWAY': object, 'LOCATION': object, 'HEIGHT': float, 'SPEED': float, 'DISTANCE': float, 
+                            'PHASE_OF_FLT': object, 'DAMAGE': object, 'STR_RAD': int, 'DAM_RAD': int, 'STR_WINDSHLD': int, 
+                            'DAM_WINDSHLD': int, 'STR_NOSE': int, 'DAM_NOSE': int, 'STR_ENG1': int, 'DAM_ENG1': int, 
+                            'STR_ENG2': int, 'DAM_ENG2': int, 'STR_ENG3': int, 'DAM_ENG3': int, 'STR_ENG4': int, 
+                            'DAM_ENG4': int, 'INGESTED': int, 'STR_PROP': int, 'DAM_PROP': int, 'STR_WING_ROT': int, 
+                            'DAM_WING_ROT': int, 'STR_FUSE': int, 'DAM_FUSE': int, 'STR_LG': int, 'DAM_LG': int,
+                            'STR_TAIL': int, 'DAM_TAIL': int, 'STR_LGHTS': int, 'DAM_LGHTS': int, 'STR_OTHER': int,
+                            'DAM_OTHER': int, 'OTHER_SPECIFY': object, 'EFFECT': object, 'EFFECT_OTHER': object, 'SKY': object,
+                            'PRECIP': object, 'SPECIES_ID': object, 'SPECIES': object, 'BIRDS_SEEN': object, 'BIRDS_STRUCK': object,
+                            'SIZE': object, 'WARNED': object, 'COMMENTS': object, 'REMARKS': object, 'AOS': float,
+                            'COST_REPAIRS': float, 'COST_OTHER': float, 'COST_REPAIRS_INFL_ADJ': float, 'COST_OTHER_INFL_ADJ': float, 'REPORTED_NAME': object,
+                            'REPORTED_TITLE': object, 'REPORTED_DATE': object, 'SOURCE': object, 'PERSON': object, 'NR_INJURIES': float,
+                            'NR_FATALITIES': float, 'LUPDATE': object, 'TRANSFER': int, 'INDICATED_DAMAGE': int})
+    return df
 
-#replaces or add names to deal with wrong data
-df.loc[df['OPID'] == "ASY", "OPERATOR"] = "Royal Australian Air Force"
-df.loc[df['OPID'] == "FDY", "OPERATOR"] = "Sun Air International"
-df.loc[df['OPID'] == "LTD", "OPERATOR"] = "Executive Express Aviation/JA Air Charter"
-df.loc[df['OPID'] == "SOI", "OPERATOR"] = "Southern Aviation"
-df.loc[df['OPID'] == "B-717IT", "OPERATOR"] = np.nan
-df.loc[df['AIRPORT_ID'] == "SPANISH PEAKS AIRFIELD", "AIRPORT"] = "SPANISH PEAKS AIRFIELD"
-df['AIRPORT_ID'].replace('SPANISH PEAKS AIRFIELD', "4V1")
-df.loc[df['AIRPORT_ID'] == "KLUK", "AIRPORT"] = "CINCINNATI MUNICIPAL"
-df.loc[df['AIRPORT_ID'] == "KDKK", "AIRPORT"] = "CHAUTAUQUA-DUNKIRK"
+def fillMissingWithNA(df): #replaces bad values with nan
+    df = df.replace('', np.nan, regex=True)
+    df = df.replace('UNKNOWN', np.nan, regex=True)
+    df = df.replace('[Uu]nknown', np.nan, regex=True)
+    df = df.replace('UNK', np.nan, regex=True)
+    df = df.replace('CHANGE CODE', np.nan, regex=True)
+    df = df.replace('ZZZZ', np.nan, regex=True)
+    return df
 
-#Converts object values to int
-df['WARNED'] = df['WARNED'].map({'Y': 1, 'N': 0, 'y': 1, 'n': 0})
+def dropValues(df): #drops not needed columns
+    df.dropna(subset=['INCIDENT_DATE'], inplace=True) #drop nan rows in INCIDENT_DATE
+    
+    df = df.drop(['INDEX_NR', 'REG', 'FLT', 'INCIDENT_MONTH', 'INCIDENT_YEAR', 'COMMENTS',  #Information here is compleatly useless
+                  'TRANSFER', 'LUPDATE', 'REPORTED_NAME', 'REPORTED_TITLE'], axis=1)
+    '''
+    removed:
+        INDEX_NR- Individual record number
+        REG- Aircraft registration
+        FLT- Flight number
+        INCIDENT_MONTH- already provided in INCIDENT_DATE
+        INCIDENT_YEAR- already provided in INCIDENT_DATE
+        COMMENTS- As entered by database manager. Can include name of aircraft owner, types of reports received, updates, etc.
+        TRANSFER- Unused field at this time
+        LUPDATE- Last time record was updated
+        REPORTED_NAME- always empty
+        REPORTED_TITLE- always empty
+    '''
+    df = df.drop(['OPID', 'AMO', 'EMO', 'ENG_1_POS', 'ENG_2_POS', 'ENG_3_POS', 'ENG_4_POS', #Information here could be used later if needed
+                 'REMAINS_COLLECTED', 'REMAINS_SENT', 'AIRPORT_ID', 'FAAREGION', 'RUNWAY', 
+                 'LOCATION', 'OTHER_SPECIFY', 'EFFECT_OTHER', 'SPECIES_ID', 'REMARKS', 
+                 'COST_REPAIRS_INFL_ADJ', 'COST_OTHER_INFL_ADJ', 'REPORTED_DATE', 'SOURCE', 
+                 'PERSON'], axis=1)
+    '''
+    removed:
+        OPID- Airline operator code
+        AMO- International Civil Aviation Organization code for Aircraft Model 
+        EMO- Engine Model Code
+        ENG_1-4_POS- Where engine is mounted  on aircraft
+        REMAINS_COLLECTED- Indicates if bird or wildlife remains were found and collected
+        REMAINS_SENT- Indicates if remains were sent to the Smithsonian Institution for identifcation
+        AIRPORT_ID- International Civil Aviation Organization airport identifier for location of strike whether it was on or off airport
+        FAAREGION- FAA Region where airport is located
+        RUNWAY- Runway
+        LOCATION- Various information about aircraft location if enroute or airport where strike evidence was found. Some locations show the two airports for the flight departure and arrival if pilot was unaware of the strike.
+        OTHER_SPECIFY- What part was struck other than those listed above
+        EFFECT_OTHER- Effect on flight other than those listed on the form
+        SPECIES_ID- International Civil Aviation Organization code for type of bird or other wildlife
+        REMARKS- Most of remarks are from the form but some are data entry notes and are usually in parentheses.
+        COST_REPAIRS_INFL_ADJ- Costs adjusted for inflation (can be self calculated)
+        COST_OTHER_INFL_ADJ- Other cost adjusted for inflation (can be self calculated)
+        REPORTED_DATE- Date report was written
+        SOURCE- Type of report. Note: for multiple types of reports this will be indicated as Multiple.  See "Comments" field for details
+        PERSON- Only one selection allowed. For multiple reports, see field "Reported Title"
+    '''
+    return df
 
-#replaces values so they are easyer to understand and use
-df['DAMAGE'] = df['DAMAGE'].replace('M?', "?")
-df['SIZE'] = df['SIZE'].str.lower().replace(r'small', "Small")
-df['SIZE'] = df['SIZE'].str.lower().replace(r'medium', "Medium")
-df['SIZE'] = df['SIZE'].str.lower().replace(r'large', "Large")
-df['SPECIES'] = (df['SPECIES'].str.lower()).str.title()
-df['EFFECT'] = (df['EFFECT'].str.lower()).str.title()
-df['SKY'] = df['SKY'].str.replace(r'S[Oo]me Clouds?', "Some Clouds")
-df['SKY'] = df['SKY'].str.replace(r'N[Oo] C[Ll]ouds?s?', "No Clouds")
-df['PRECIP'] = df['PRECIP'].replace('NoNe', "None")
-df['EFFECT'] = df['EFFECT'].replace('NONE', "None")
-df['PHASE_OF_FLT'] = df['PHASE_OF_FLT'].replace('take-off run', "Take-off run")
-df['PHASE_OF_FLT'] = df['PHASE_OF_FLT'].str.replace('landing roll', "Landing roll")
-df['PHASE_OF_FLT'] = df['PHASE_OF_FLT'].replace('approach', "Approach")
-df['PHASE_OF_FLT'] = df['PHASE_OF_FLT'].replace('climb', "Climb")
-df['PHASE_OF_FLT'] = df['PHASE_OF_FLT'].replace('descent', "Descent")
+def replaceNA(df): #replaces or add names to deal with wrong data
+    df.loc[df['OPID'] == "ASY", "OPERATOR"] = "Royal Australian Air Force"
+    df.loc[df['OPID'] == "FDY", "OPERATOR"] = "Sun Air International"
+    df.loc[df['OPID'] == "LTD", "OPERATOR"] = "Executive Express Aviation/JA Air Charter"
+    df.loc[df['OPID'] == "SOI", "OPERATOR"] = "Southern Aviation"
+    df.loc[df['OPID'] == "B-717IT", "OPERATOR"] = np.nan
+    df.loc[df['AIRPORT_ID'] == "SPANISH PEAKS AIRFIELD", "AIRPORT"] = "SPANISH PEAKS AIRFIELD"
+    df['AIRPORT_ID'].replace('SPANISH PEAKS AIRFIELD', "4V1")
+    df.loc[df['AIRPORT_ID'] == "KLUK", "AIRPORT"] = "CINCINNATI MUNICIPAL"
+    df.loc[df['AIRPORT_ID'] == "KDKK", "AIRPORT"] = "CHAUTAUQUA-DUNKIRK"
+    return df
 
-#Converts object to datetime
-df['INCIDENT_DATE'] = pd.to_datetime(df['INCIDENT_DATE'])
+def convertObjToType(df): 
+    df['WARNED'] = df['WARNED'].map({'Y': 1, 'N': 0, 'y': 1, 'n': 0}) #Converts object values to int
+    df['INCIDENT_DATE'] = pd.to_datetime(df['INCIDENT_DATE']) #Converts object to datetime
+    return df
 
-#drop nan rows in INCIDENT_DATE
-df.dropna(subset=['INCIDENT_DATE'], inplace=True)
+def replaceValuesForEase(df): #replaces values so they are easyer to understand and use
+    df['DAMAGE'] = df['DAMAGE'].replace('M?', "?")
+    df['SIZE'] = df['SIZE'].str.lower().replace(r'small', "Small")
+    df['SIZE'] = df['SIZE'].str.lower().replace(r'medium', "Medium")
+    df['SIZE'] = df['SIZE'].str.lower().replace(r'large', "Large")
+    df['SPECIES'] = (df['SPECIES'].str.lower()).str.title()
+    df['EFFECT'] = (df['EFFECT'].str.lower()).str.title()
+    df['SKY'] = df['SKY'].str.replace(r'S[Oo]me Clouds?', "Some Clouds")
+    df['SKY'] = df['SKY'].str.replace(r'N[Oo] C[Ll]ouds?s?', "No Clouds")
+    df['PRECIP'] = df['PRECIP'].replace('NoNe', "None")
+    df['EFFECT'] = df['EFFECT'].replace('NONE', "None")
+    df['PHASE_OF_FLT'] = df['PHASE_OF_FLT'].replace('take-off run', "Take-off run")
+    df['PHASE_OF_FLT'] = df['PHASE_OF_FLT'].str.replace('landing roll', "Landing roll")
+    df['PHASE_OF_FLT'] = df['PHASE_OF_FLT'].replace('approach', "Approach")
+    df['PHASE_OF_FLT'] = df['PHASE_OF_FLT'].replace('climb', "Climb")
+    df['PHASE_OF_FLT'] = df['PHASE_OF_FLT'].replace('descent', "Descent")
+    return df
 
-#fixes INCIDENT_DATE based on given info
-times = {'Night': '23:59', 'Dusk': '18:00', 'Day': '12:00', 'Dawn': '6:00'}
-df['TIME_OF_DAY'] = df['TIME_OF_DAY'].apply(times.get)
-df['TIME'] = pd.to_datetime((df['TIME'].abs() // 100 * 60) + (df['TIME'].abs() % 100), unit='m')
-df['TIME'] = df['TIME'].astype(str).str.extract('(..:..)', expand=True)
-df['TIME'].fillna(df['TIME_OF_DAY'], inplace=True)
-df['TIME'] = df['TIME'].replace(np.nan, '0:00', regex=True)
-df['INCIDENT_DATE'] = pd.to_datetime(df['INCIDENT_DATE'].apply(str)+' '+df['TIME'])
-df = df.drop(['TIME'], axis=1)
-df = df.drop(['TIME_OF_DAY'], axis=1)
+def fixIncidentDate(df): #fixes INCIDENT_DATE based on given info
+    times = {'Night': '23:59', 'Dusk': '18:00', 'Day': '12:00', 'Dawn': '6:00'}
+    df['TIME_OF_DAY'] = df['TIME_OF_DAY'].apply(times.get)
+    df['TIME'] = pd.to_datetime((df['TIME'].abs() // 100 * 60) + (df['TIME'].abs() % 100), unit='m')
+    df['TIME'] = df['TIME'].astype(str).str.extract('(..:..)', expand=True)
+    df['TIME'].fillna(df['TIME_OF_DAY'], inplace=True)
+    df['TIME'] = df['TIME'].replace(np.nan, '0:00', regex=True)
+    df['INCIDENT_DATE'] = pd.to_datetime(df['INCIDENT_DATE'].apply(str)+' '+df['TIME'])
+    df = df.drop(['TIME'], axis=1)
+    df = df.drop(['TIME_OF_DAY'], axis=1)
+    return df
 
-#drops not needed columns:
-#Information here is compleatly useless
-df = df.drop(['INDEX_NR', 'REG', 'FLT', 'INCIDENT_MONTH', 'INCIDENT_YEAR', 'COMMENTS', 
-              'TRANSFER', 'LUPDATE', 'REPORTED_NAME', 'REPORTED_TITLE'], axis=1)
+########################################## SORT DF ####################################
+def sortDf(df): #Sorts values based on date and sets INCIDENT_DATE as index
+    df = df.sort_values('INCIDENT_DATE')
+    df = df.set_index('INCIDENT_DATE')
+    return df
 
-'''
-removed:
-    INDEX_NR- Individual record number
-    REG- Aircraft registration
-    FLT- Flight number
-    INCIDENT_MONTH- already provided in INCIDENT_DATE
-    INCIDENT_YEAR- already provided in INCIDENT_DATE
-    COMMENTS- As entered by database manager. Can include name of aircraft owner, types of reports received, updates, etc.
-    TRANSFER- Unused field at this time
-    LUPDATE- Last time record was updated
-    REPORTED_NAME- always empty
-    REPORTED_TITLE- always empty
-'''
-#Information here could be used later if needed
-df = df.drop(['OPID', 'AMA', 'AMO','EMA', 'EMO', 'ENG_1_POS', 'ENG_2_POS', 'ENG_3_POS', 'ENG_4_POS',
-              'REMAINS_COLLECTED', 'REMAINS_SENT', 'AIRPORT_ID', 'FAAREGION', 'RUNWAY', 
-              'LOCATION', 'OTHER_SPECIFY', 'EFFECT_OTHER', 'SPECIES_ID', 'REMARKS', 
-              'COST_REPAIRS_INFL_ADJ', 'COST_OTHER_INFL_ADJ', 'REPORTED_DATE', 'SOURCE', 
-              'PERSON'], axis=1)
-'''
-removed:
-    OPID- Airline operator code
-    AMA- International Civil Aviation Organization code for Aircraft Make
-    AMO- International Civil Aviation Organization code for Aircraft Model 
-    EMA- Engine Make Code
-    EMO- Engine Model Code
-    ENG_1-4_POS- Where engine is mounted  on aircraft
-    REMAINS_COLLECTED- Indicates if bird or wildlife remains were found and collected
-    REMAINS_SENT- Indicates if remains were sent to the Smithsonian Institution for identifcation
-    AIRPORT_ID- International Civil Aviation Organization airport identifier for location of strike whether it was on or off airport
-    FAAREGION- FAA Region where airport is located
-    RUNWAY- Runway
-    LOCATION- Various information about aircraft location if enroute or airport where strike evidence was found. Some locations show the two airports for the flight departure and arrival if pilot was unaware of the strike.
-    OTHER_SPECIFY- What part was struck other than those listed above
-    EFFECT_OTHER- Effect on flight other than those listed on the form
-    SPECIES_ID- International Civil Aviation Organization code for type of bird or other wildlife
-    REMARKS- Most of remarks are from the form but some are data entry notes and are usually in parentheses.
-    COST_REPAIRS_INFL_ADJ- Costs adjusted for inflation (can be self calculated)
-    COST_OTHER_INFL_ADJ- Other cost adjusted for inflation (can be self calculated)
-    REPORTED_DATE- Date report was written
-    SOURCE- Type of report. Note: for multiple types of reports this will be indicated as Multiple.  See "Comments" field for details
-    PERSON- Only one selection allowed. For multiple reports, see field "Reported Title"
-'''
-
-#Sorts values based on date and sets INCIDENT_DATE as index
-df = df.sort_values('INCIDENT_DATE')
-df = df.set_index('INCIDENT_DATE')
-
-#Writes to new file
-df.to_csv('wildlife-collisions-Modified.csv', encoding = "utf-8")
-
-print('Done!')
+########################################## MAIN FUNCTION ####################################
+def main():
+    os.chdir(find('wildlife-collisions.csv')) #set working directory
+    df=readFiles()
+    df=fillMissingWithNA(df)
+    df=replaceNA(df)
+    df=convertObjToType(df)
+    df=dropValues(df)
+    df=replaceValuesForEase(df)
+    df=fixIncidentDate(df)
+    df=sortDf(df)
+    df.to_csv(path_or_buf='wildlife-collisions-Modified.csv', encoding = "utf-8") #Writes to new file
+    print('Done!')
+    
+if __name__ == '__main__': #when program starts, start with main function
+    main()
