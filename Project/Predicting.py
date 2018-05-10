a6cb564aa798f25ee489d495a8ce9264d0d0d6b5
bubriks <bubriks@gmail.com>
2018-05-10 19:17:22 +0200

    Predictive analysis part

--- Project/Predicting.py
+++ Project/Predicting.py
@@ -47,22 +47,6 @@ model.fit(X_train, y_train)
 accuracy = model.score(X_test, y_test)
 print(accuracy)
 
-#takes long time and shows how the values group up
-from sklearn.cluster import KMeans
-'''
-X_train, X_test = cross_validation.train_test_split(X, test_size=0.3, random_state=7)
-
-model = KMeans(n_clusters=10, random_state=7)
-model.fit(X_train)
-
-predicted= model.predict(X_test)
-
-unique_elements, counts_elements = np.unique(predicted, return_counts=True)
-print("Frequency of unique values of the said array:")
-print(np.asarray((unique_elements, counts_elements)))
-
-pd.DataFrame(predicted).hist()
-'''
 '''
 #Show corolations
 from mpl_toolkits.mplot3d import Axes3D 
@@ -70,7 +54,7 @@ import matplotlib.pyplot as plt
 
 X = np.array(df[['MONTH', 'CRASHED', 'DAY_OF_WEEK']])
 
-est = KMeans(n_clusters=7)
+est = KMeans(n_clusters=3)
 est.fit(X)
 
 ax = Axes3D(plt.figure(1, figsize=(4, 3)), rect=[0, 0, .95, 1], elev=48, azim=134)
@@ -81,3 +65,59 @@ ax.set_ylabel('CRASHED')
 ax.set_zlabel('DAY_OF_WEEK')
 plt.show()
 '''
+
+import matplotlib.pyplot as plt
+
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
+
+data = scale(X)
+
+# #############################################################################
+# Visualize the results on PCA-reduced data
+
+reduced_data = PCA(n_components=2).fit_transform(data)
+kmeans = KMeans(init='k-means++', n_clusters=10, n_init=10)
+kmeans.fit(reduced_data)
+
+# Step size of the mesh. Decrease to increase the quality of the VQ.
+h = .02
+
+# Plot the decision boundary. For that, we will assign a color to each
+x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
+y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+# Obtain labels for each point in mesh. Use last trained model.
+Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
+
+unique_elements, counts_elements = np.unique(Z, return_counts=True)
+print("Frequency of unique values of the said array:")
+#Show in which closter how many of X_test values would be added
+print(np.asarray((unique_elements, counts_elements)))
+
+pd.DataFrame(Z).hist()
+
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+plt.figure(1)
+plt.clf()
+plt.imshow(Z, interpolation='nearest',
+           extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+           cmap=plt.cm.Paired,
+           aspect='auto', origin='lower')
+
+plt.plot(reduced_data[:, 0], reduced_data[:, 1], 'k.', markersize=2)
+# Plot the centroids as a white X
+centroids = kmeans.cluster_centers_
+plt.scatter(centroids[:, 0], centroids[:, 1],
+            marker='x', s=169, linewidths=3,
+            color='w', zorder=10)
+plt.title('K-means clustering on the dataset\n'
+          'Centroids are marked with white cross')
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+plt.xticks(())
+plt.yticks(())
+plt.show()
