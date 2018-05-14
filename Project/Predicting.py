e989604111b5d8b77c4bfb732953a943f64a5dcc
bubriks <bubriks@gmail.com>
2018-05-14 16:07:13 +0200

    Prediction part should be done

--- Project/Predicting.py
+++ Project/Predicting.py
@@ -6,6 +6,12 @@ This file represents the prediction part organized according to BigData3 lessons
 import os
 import pandas as pd 
 import numpy as np
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn import tree
+from sklearn import cross_validation
 pd.options.mode.chained_assignment = None  # default='warn'
 
 os.chdir('dataset/Modified_dataset/') #set working directory
@@ -24,65 +30,10 @@ types = {'MONTH': int, 'DAY': int, 'DAY_OF_WEEK': int, 'AIRLINE': object,
 df = pd.read_csv('wildlife-collisions_Joined2015.csv',
                  dtype=types)
 
-
-##########################################################################
-#shows how data was clustared
-import pandas as pd
-from scipy import stats
-from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-df = pd.get_dummies(df, columns=['CANCELLATION_REASON'])
-
-#Standardize
-#removed: AIRLINE, FLIGHT_NUMBER, TAIL_NUMBER
-#ORIGIN_AIRPORT DESTINATION_AIRPORT
-clmns = ['MONTH', 'DAY',
-         'DEPARTURE_TIME', 'ARRIVAL_TIME', 'DAY_OF_WEEK',
-         'SCHEDULED_DEPARTURE', 'DEPARTURE_DELAY', 'TAXI_OUT', 
-         'WHEELS_OFF', 'SCHEDULED_TIME', 'ELAPSED_TIME', 
-         'AIR_TIME', 'DISTANCE', 'WHEELS_ON', 'TAXI_IN',
-         'SCHEDULED_ARRIVAL', 'ARRIVAL_DELAY', 'DIVERTED', 
-         'CANCELLED', 'AIR_SYSTEM_DELAY', 
-         'SECURITY_DELAY', 'AIRLINE_DELAY', 'LATE_AIRCRAFT_DELAY', 
-         'WEATHER_DELAY', 'CRASHED', 
-         'CANCELLATION_REASON_0', 'CANCELLATION_REASON_B', 'CANCELLATION_REASON_A', 
-         'CANCELLATION_REASON_C', 'CANCELLATION_REASON_D']
-df_tr_std = stats.zscore(df[clmns])
-
-#Cluster the data
-kmeans = KMeans(n_clusters=3, random_state=0).fit(df_tr_std)
-labels = kmeans.labels_
-
-#Glue back to originaal data
-df['clusters'] = labels
-
-#Add the column into our list
-clmns.extend(['clusters'])
-
-#Lets analyze the clusters
-print(df[clmns].groupby(['clusters']).mean())
-
-sns.lmplot('DEPARTURE_TIME', 'ARRIVAL_TIME', 
-           data=df, 
-           fit_reg=False, 
-           hue="clusters",  
-           scatter_kws={"marker": "D", 
-                        "s": 100})
-plt.title('Clusters')
-plt.xlabel('DEPARTURE_TIME')
-plt.ylabel('ARRIVAL_TIME')
-
-'''
-############################################################
 #Supervised learning using the decision tree
 #X- feature
 #y- lable
 
-from sklearn import tree
-from sklearn import cross_validation
-#df_tr = pd.get_dummies(df_tr, columns=['timeOfDay'])
 #'AIRLINE', 'FLIGHT_NUMBER', 'TAIL_NUMBER', 'ORIGIN_AIRPORT', 'DESTINATION_AIRPORT', 'CANCELLATION_REASON'
 X = df[['MONTH', 'DAY', 'DEPARTURE_TIME', 'ARRIVAL_TIME', 'DAY_OF_WEEK',
          'SCHEDULED_DEPARTURE', 'DEPARTURE_DELAY', 'TAXI_OUT', 
@@ -94,108 +45,55 @@ X = df[['MONTH', 'DAY', 'DEPARTURE_TIME', 'ARRIVAL_TIME', 'DAY_OF_WEEK',
          'WEATHER_DELAY']]
 y = df[['CRASHED']]
 
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X[:-10], y[:-10], test_size=0.3, random_state=7)
+predict = 100
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X[:-predict], y[:-predict], test_size=0.3, random_state=7)
 model = tree.DecisionTreeClassifier()
 model.fit(X_train, y_train)
 accuracy = model.score(X_test, y_test)
 print(accuracy)
 
-print(model.predict(X[-10:]))#last 10
-print(y[-10:])
-'''
-'''
-#helpful
-import matplotlib.pyplot as plt  
-import numpy as np  
-from sklearn.cluster import KMeans  
+print(model.predict(X[-predict:]))
+print(y[-predict:])
 
-X = np.array([[5,3],  
-     [10,15],
-     [15,12],
-     [24,10],
-     [30,45],
-     [85,70],
-     [71,80],
-     [60,78],
-     [55,52],
-     [80,91],])
+#############################################################################################
+types = {'INCIDENT_DATE': object, 'OPERATOR': object, 'ATYPE': object, 
+         'AC_CLASS': object, 'AC_MASS': int, 'NUM_ENGS': int, 
+         'TYPE_ENG': object, 'AIRPORT': object, 'STATE': object, 
+         'HEIGHT': float, 'SPEED': float, 'DISTANCE': float, 'PHASE_OF_FLT': object, 
+         'STR_RAD	DAM_RAD': int, 'STR_WINDSHLD': int, 'DAM_WINDSHLD': int,
+         'STR_NOSE': int, 'DAM_NOSE': int, 'STR_ENG1': int, 'DAM_ENG1': int, 'STR_ENG2': int,
+         'DAM_ENG2': int, 'STR_ENG3': int, 'DAM_ENG3': int, 'STR_ENG4': int,
+         'DAM_ENG4': int, 'INGESTED': int, 'STR_PROP': int, 'DAM_PROP': int, 'STR_WING_ROT': int,	
+         'DAM_WING_ROT': int, 'STR_FUSE': int, 'DAM_FUSE': int, 'STR_LG': int, 'DAM_LG': int,
+         'STR_TAIL': int, 'DAM_TAIL': int, 'STR_LGHTS': int, 
+         'DAM_LGHTS': int, 'STR_OTHER': int, 'DAM_OTHER': int,
+         'EFFECT': object, 'SKY': object, 'PRECIP': object, 'SPECIES': object, 'BIRDS_STRUCK': object,
+         'SIZE': object, 'AOS': float, 'NR_INJURIES': int, 'NR_FATALITIES': int, 'INDICATED_DAMAGE': int,	'COSTS': float}
 
-kmeans = KMeans(n_clusters=2)  
-kmeans.fit(X)  
-print(kmeans.cluster_centers_)  
-print(kmeans.labels_) 
-plt.scatter(X[:,0],X[:,1], c=kmeans.labels_, cmap='rainbow')  
-plt.scatter(kmeans.cluster_centers_[:,0] ,kmeans.cluster_centers_[:,1], color='black')  
-'''
-'''
-#######################################################
-#Shows unsupervised learning- k means
-import matplotlib.pyplot as plt
+df = pd.read_csv('wildlife-collisions.csv',
+                 dtype=types)
 
-from sklearn.cluster import KMeans
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import scale
+df['INCIDENT_DATE'] = df['INCIDENT_DATE'].astype(str).str.extract('(....-..-..)', expand=True)
+df['Year'], df['Month'], df['Day'] = df['INCIDENT_DATE'].str.split('-').str
+df = df.drop(['INCIDENT_DATE'], axis=1)
 
-data = scale(df[['MONTH', 'DAY', 'DEPARTURE_TIME', 'ARRIVAL_TIME', 'DAY_OF_WEEK',
-         'SCHEDULED_DEPARTURE', 'DEPARTURE_DELAY', 'TAXI_OUT', 
-         'WHEELS_OFF', 'SCHEDULED_TIME', 'ELAPSED_TIME', 
-         'AIR_TIME', 'DISTANCE', 'WHEELS_ON', 'TAXI_IN',
-         'SCHEDULED_ARRIVAL', 'ARRIVAL_DELAY', 'DIVERTED', 
-         'CANCELLED', 'AIR_SYSTEM_DELAY', 'FLIGHT_NUMBER', 
-         'SECURITY_DELAY', 'AIRLINE_DELAY', 'LATE_AIRCRAFT_DELAY', 
-         'WEATHER_DELAY', 'CRASHED']])
+df = pd.get_dummies(df, columns=['AC_CLASS'])
+df = pd.get_dummies(df, columns=['STATE'])
+df = pd.get_dummies(df, columns=['TYPE_ENG'])
+df = pd.get_dummies(df, columns=['PHASE_OF_FLT'])
+df = pd.get_dummies(df, columns=['EFFECT'])
+df = pd.get_dummies(df, columns=['SKY'])
+df = pd.get_dummies(df, columns=['PRECIP'])
+df = pd.get_dummies(df, columns=['BIRDS_STRUCK'])
+df = pd.get_dummies(df, columns=['SIZE'])
 
-# Visualize the results on PCA-reduced data
-reduced_data = PCA(n_components=2).fit_transform(data)
-kmeans = KMeans(init='k-means++', n_clusters=3, n_init=10)
-kmeans.fit(reduced_data)
+df = df.drop(['OPERATOR', 'ATYPE', 'AIRPORT', 'SPECIES'], axis=1)
 
-# Plot the decision boundary. For that, we will assign a color to each
-x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
-y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
-xx, yy = np.meshgrid(np.arange(x_min, x_max, .02), np.arange(y_min, y_max, .02))
-
-# Obtain labels for each point in mesh. Use last trained model.
-Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
-
-unique_elements, counts_elements = np.unique(Z, return_counts=True)
-
-#print(np.asarray((unique_elements, counts_elements)))
-#pd.DataFrame(Z).hist()
-
-# Put the result into a color plot
-Z = Z.reshape(xx.shape)
-plt.figure(1)
-plt.clf()
-plt.imshow(Z, interpolation='nearest',
-           extent=(xx.min(), xx.max(), yy.min(), yy.max()),
-           cmap=plt.cm.Paired,
-           aspect='auto', origin='lower')
-
-plt.plot(reduced_data[:, 0], reduced_data[:, 1], 'k.', markersize=2)
-
-# Plot the centroids as a white X
-plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1],
-            marker='x', s=169, linewidths=3,
-            color='w', zorder=10)
-
-plt.title('K-means clustering on the dataset')
-plt.xlim(x_min, x_max)
-plt.ylim(y_min, y_max)
-plt.xticks(())
-plt.yticks(())
-plt.show()
-'''
-
-'''
-##################################################################
 #Deciding how many cluster are needed by using the Elbow Analysis
-from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
 
 scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
+X_scaled = scaler.fit_transform(df)
 
 cluster_range  = range( 1, 10 )
 cluster_errors = []
@@ -213,4 +111,31 @@ print(clusters_df)
 plt.figure(figsize=(12,6))
 plt.plot( clusters_df.num_clusters, clusters_df.cluster_errors, marker = "o" )
 plt.show()
-'''
+
+#Unsupervised learning using k means
+predict = 100
+
+reduced_data = PCA(n_components=2).fit_transform(df[:-predict])
+kmeans = KMeans(init='k-means++', n_clusters=5, n_init=10).fit(reduced_data)
+df['clusters'] = np.nan
+df['clusters'][:-predict] = kmeans.labels_
+
+#Analyze the clusters
+print(df.groupby(['clusters']).mean())
+unique_elements, counts_elements = np.unique(kmeans.labels_, return_counts=True)
+print(np.asarray((unique_elements, counts_elements)))
+
+#Visualization of actual data
+plt.scatter(reduced_data[:,0], reduced_data[:,1], c=kmeans.labels_, cmap='rainbow')  
+plt.scatter(kmeans.cluster_centers_[:,0] ,kmeans.cluster_centers_[:,1], marker='x', color='w')   
+plt.title('K-means clustering on the wildlife-collisions dataset')
+plt.xticks(())
+plt.yticks(())
+plt.show()
+
+#Prediction
+reduced_data = PCA(n_components=2).fit_transform(df.loc[:, df.columns != 'clusters'][-predict:])
+Z = kmeans.predict(reduced_data)
+df['clusters'][-predict:] = Z
+unique_elements, counts_elements = np.unique(Z, return_counts=True)
+print(np.asarray((unique_elements, counts_elements)))
