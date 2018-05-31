18fd98d0ecd2ef7e70bc824afb9b24b227ba71da
bubriks <bubriks@gmail.com>
2018-05-31 15:09:16 +0200

    test

--- Project/Predicting.py
+++ Project/Predicting.py
@@ -8,7 +8,6 @@ import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn import tree
 from sklearn import cross_validation
@@ -30,26 +29,18 @@ types = {'MONTH': int, 'DAY': int, 'DAY_OF_WEEK': int, 'AIRLINE': object,
 df = pd.read_csv('wildlife-collisions_Joined2015.csv',
                  dtype=types)
 
-'''
-#df = pd.get_dummies(df, columns=['SIZE'])
-
-not added:
-'AIRLINE' 'FLIGHT_NUMBER' 'TAIL_NUMBER' 'ORIGIN_AIRPORT' 'DESTINATION_AIRPORT' 
-'CANCELLATION_REASON' 'CANCELLED' 'ORIGIN_STATE' 'DESTINATION_STATE'
-
-use info that we will know before hand
-'''
-
-df = df[['MONTH', 'DAY', 'DEPARTURE_TIME', 'ARRIVAL_TIME', 'DAY_OF_WEEK',
+#Deciding how many cluster are needed by using the Elbow Analysis
+def ElbowAnalysis(df):
+    print('Elbow Analysis')
+    
+    df = df[['MONTH', 'DAY', 'DEPARTURE_TIME', 'ARRIVAL_TIME', 'DAY_OF_WEEK',
          'SCHEDULED_DEPARTURE', 'DEPARTURE_DELAY', 'TAXI_OUT', 
          'WHEELS_OFF', 'SCHEDULED_TIME', 'ELAPSED_TIME', 
-         'AIR_TIME', 'DISTANCE', 'WHEELS_ON', 'TAXI_IN',
+         'AIR_TIME', 'DISTANCE', 'WHEELS_ON', 'TAXI_IN', 'CANCELLED',
          'SCHEDULED_ARRIVAL', 'ARRIVAL_DELAY', 'DIVERTED', 'AIR_SYSTEM_DELAY',
          'SECURITY_DELAY', 'AIRLINE_DELAY', 'LATE_AIRCRAFT_DELAY', 
          'WEATHER_DELAY', 'CRASHED']]
-
-#Deciding how many cluster are needed by using the Elbow Analysis
-def ElbowAnalysis(df):
+    
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(df)
 
@@ -58,9 +49,8 @@ def ElbowAnalysis(df):
 
     for num_clusters in cluster_range:
         print(num_clusters)
-        clusters = KMeans( num_clusters )
-        clusters.fit( X_scaled )
-        cluster_errors.append( clusters.inertia_ )
+        clusters = KMeans(num_clusters).fit(X_scaled)
+        cluster_errors.append(clusters.inertia_)
     
     clusters_df = pd.DataFrame( { "num_clusters":cluster_range, "cluster_errors": cluster_errors } )
 
@@ -71,39 +61,45 @@ def ElbowAnalysis(df):
     plt.show()
 
 #Unsupervised learning using k means
-def Unsupervised(df, predict = 100, clusters=4):
+def Unsupervised(df, predict = 100, clusters=5):
     print('Unsupervised')
+    
+    df = df[['MONTH', 'DAY', 'DEPARTURE_TIME', 'ARRIVAL_TIME', 'DAY_OF_WEEK',
+         'SCHEDULED_DEPARTURE', 'DEPARTURE_DELAY', 'TAXI_OUT', 
+         'WHEELS_OFF', 'SCHEDULED_TIME', 'ELAPSED_TIME', 
+         'AIR_TIME', 'DISTANCE', 'WHEELS_ON', 'TAXI_IN', 'CANCELLED',
+         'SCHEDULED_ARRIVAL', 'ARRIVAL_DELAY', 'DIVERTED', 'AIR_SYSTEM_DELAY',
+         'SECURITY_DELAY', 'AIRLINE_DELAY', 'LATE_AIRCRAFT_DELAY', 
+         'WEATHER_DELAY', 'CRASHED']]
+    
     #Scaling- mean=0 std=1
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(df)
-    
-    reduced_data = PCA(n_components=2).fit_transform(X_scaled[:-predict])
-    kmeans = KMeans(init='k-means++', n_clusters=clusters, n_init=10).fit(reduced_data)
+    kmeans = KMeans(init='k-means++', n_clusters=clusters, n_init=10).fit(X_scaled[:-predict])
     df['clusters'] = np.nan
     df['clusters'][:-predict] = kmeans.labels_
-
-    #Analyze the clusters
-    print(df.groupby(['clusters']).mean())
-    unique_elements, counts_elements = np.unique(kmeans.labels_, return_counts=True)
-    print(np.asarray((unique_elements, counts_elements)))
-    '''
+    
     #Visualization of actual data
+    from sklearn.decomposition import PCA
+    reduced_data = PCA(n_components=2).fit_transform(X_scaled[:-predict])
     plt.scatter(reduced_data[:,0], reduced_data[:,1], c=kmeans.labels_, cmap='rainbow')  
     plt.scatter(kmeans.cluster_centers_[:,0] ,kmeans.cluster_centers_[:,1], marker='x', color='w')   
     plt.title('K-means clustering on the wildlife-collisions dataset')
     plt.xticks(())
     plt.yticks(())
     plt.show()
-    '''
+    
     #Prediction
-    reduced_data = PCA(n_components=2).fit_transform(X_scaled[-predict:])
-    Z = kmeans.predict(reduced_data)
+    Z = kmeans.predict(X_scaled[-predict:])
     df['clusters'][-predict:] = Z
     unique_elements, counts_elements = np.unique(Z, return_counts=True)
     print('Prediction results:')
     print(np.asarray((unique_elements, counts_elements)))
-
-    return df
+    
+    #Analyze the clusters
+    print(df.groupby(['clusters']).mean())
+    unique_elements, counts_elements = np.unique(kmeans.labels_, return_counts=True)
+    print(np.asarray((unique_elements, counts_elements)))
 
 
 #Supervised learning using the decision tree
@@ -111,11 +107,25 @@ def Unsupervised(df, predict = 100, clusters=4):
 #y- lable
 def Supervised(df, predict = 100, Column_y = 'CRASHED'):
     print('Supervised')
+    
+    df = df[['MONTH', 'DAY', 'DAY_OF_WEEK', 'SCHEDULED_DEPARTURE', 
+         'SCHEDULED_TIME', 'SCHEDULED_ARRIVAL', 'DISTANCE',
+         'CRASHED', 'ORIGIN_STATE', 'DESTINATION_STATE']]
+    
     X = df.drop([Column_y], axis=1)
+    
+    from sklearn.preprocessing import LabelEncoder
+    enc = LabelEncoder()
+    label_encoder = enc.fit(X[['ORIGIN_STATE']])
+    X[['ORIGIN_STATE']] = label_encoder.transform(X[['ORIGIN_STATE']])
+    label_encoder = enc.fit(X[['DESTINATION_STATE']])
+    X[['DESTINATION_STATE']] = label_encoder.transform(X[['DESTINATION_STATE']])
+    
     y = df[[Column_y]]
 
     X_train, X_test, y_train, y_test = cross_validation.train_test_split(X[:-predict], y[:-predict], test_size=0.3, random_state=7)
-    model = tree.DecisionTreeClassifier(random_state=17, min_impurity_decrease=0.000001)
+    model = tree.DecisionTreeClassifier(random_state=17, max_leaf_nodes  = 150)
+    #min_impurity_decrease = 0.0000002
     model.fit(X_train, y_train)
     accuracy = model.score(X_test, y_test)
     print(accuracy)
@@ -134,8 +144,7 @@ def Supervised(df, predict = 100, Column_y = 'CRASHED'):
     
 if __name__ == '__main__': #when program starts, start with main function
     #ElbowAnalysis(df)
-    df = Unsupervised(df)
-    
-    Supervised(df)
+    Unsupervised(df)
+    #Supervised(df)
     print('Done!')
     
